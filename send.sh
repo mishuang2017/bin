690f2f553fbb7108fd70007f9ab2573a7c298da9
Chris Mi <cmi@nvidia.com>
2021-01-26 19:29:18 +0800

    update

--- send.sh
+++ send.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-git send-email --dry-run /labhome/chrism/sflow/ofproto/0/r9/* --to=dev@openvswitch.org --cc=echaudro@redhat.com --cc=i.maximets@ovn.org --cc=elibr@nvidia.com -cc=roniba@nvidia.com
-# git send-email /labhome/chrism/sflow/ofproto/0/r5_2/* --to=dev@openvswitch.org --cc=i.maximets@ovn.org --cc=elibr@nvidia.com -cc=roniba@nvidia.com
+####### remove change-id and issue ########
+
+# git send-email --dry-run /labhome/cmi/sflow/ofproto/0/r11/* --to=dev@openvswitch.org --cc=echaudro@redhat.com --cc=i.maximets@ovn.org --cc=elibr@nvidia.com -cc=roniba@nvidia.com
+git send-email --dry-run /labhome/cmi/sflow/saeed/0002-net-psample-Introduce-stubs-to-remove-NIC-driver-dep.patch --to=NBU-linux-internal@nvidia.com -cc=jiri@nvidia.com --cc=saeedm@nvidia.com
+
+# git send-email /labhome/cmi/sflow/ofproto/0/r5_2/* --to=dev@openvswitch.org --cc=i.maximets@ovn.org --cc=elibr@nvidia.com -cc=roniba@nvidia.com
 # i.maximets@ovn.org
 # simon.horman@netronome.com
 # sriharsha.basavapatna@broadcom.com
