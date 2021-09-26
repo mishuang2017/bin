51f2d4179a0fdc48b4916f6864edaad7e70eef18
Chris Mi <cmi@nvidia.com>
2021-09-26 16:33:37 +0800

    update

--- local_remote_VTEPS.sh
+++ local_remote_VTEPS.sh
@@ -7,20 +7,27 @@ offload=""
 [[ "$1" == "sw" ]] && offload="skip_hw"
 
 TC=tc
+ns=n11
 link=enp4s0f0
 link2=enp4s0f1
 rep2=enp4s0f0_1
-
+vf2=enp4s0f0v1
 link_ip=192.168.1.13
 link_remote_ip=192.168.1.14
+local_vm_mac=$(ip netns exec $ns cat /sys/class/net/$vf2/address)
 link2_mac=$(cat /sys/class/net/$link2/address)
 
 vx=vxlan1
 vni=4
 vxlan_port=4789
-
-local_vm_mac=02:25:d0:13:01:02
 vxlan_mac=24:25:d0:e1:00:00
+
+ifconfig $link 0
+ifconfig $link2 0
+ifconfig $link $link_ip/16 up
+ifconfig $link2 $link_remote_ip/16 up
+arp -i $link -s $link_remote_ip $link2_mac
+ip netns exe $ns arp -i enp4s0f0v1 -s 1.1.1.200 $vxlan_mac
 
 ip link del $vx > /dev/null 2>&1
 ip link add name $vx type vxlan id $vni dev $link remote $link_remote_ip dstport $vxlan_port
@@ -40,7 +47,6 @@ $TC qdisc add dev $vx ingress
 ip link set $link promisc on
 ip link set $rep2 promisc on
 ip link set $vx promisc on
-
 
 $TC filter add dev $rep2 protocol ip  parent ffff: prio 1 flower $offload \
 	src_mac $local_vm_mac           \
@@ -77,12 +83,5 @@ $TC filter add dev $vx protocol arp parent ffff: prio 2 flower skip_hw  \
 	enc_key_id $vni                 \
 	action tunnel_key unset         \
 	action mirred egress redirect dev $rep2
-
-ifconfig $link 0
-ifconfig $link2 0
-ifconfig $link $link_ip/16 up
-ifconfig $link2 $link_remote_ip/16 up
-arp -i $link -s $link_remote_ip $link2_mac
-ip netns exe n11 arp -i enp4s0f0v1 -s 1.1.1.200 $vxlan_mac
 
 set +x
