21d7df9f5210a214aa5292b5593a84f3817458ec
Chris Mi <cmi@nvidia.com>
2021-12-17 07:39:49 +0200

    update

--- test_router-dnat-ct-only.sh
+++ test_router-dnat-ct-only.sh
@@ -103,17 +103,6 @@ ip netns exec n11 ip route add 8.9.10.0/24 via $VM_ROUTE_IP dev $VF
 ovs-ofctl add-flow $BR_INT "table=0, in_port=$REP, dl_type=0x0806, nw_dst=$VM_ROUTE_IP, actions=load:0x2->NXM_OF_ARP_OP[], move:NXM_OF_ETH_SRC[]->NXM_OF_ETH_DST[], mod_dl_src=${ROUTE_MAC}, move:NXM_NX_ARP_SHA[]->NXM_NX_ARP_THA[], move:NXM_OF_ARP_SPA[]->NXM_OF_ARP_TPA[], load:$ROUTE_MAC_HEX->NXM_NX_ARP_SHA[], load:$VM_ROUTE_IP_HEX->NXM_OF_ARP_SPA[], in_port"
 ovs-ofctl add-flow $BR_INT "table=0, in_port=$PATCH_INT, dl_type=0x0806, nw_dst=$ROUTE_IP, actions=load:0x2->NXM_OF_ARP_OP[], move:NXM_OF_ETH_SRC[]->NXM_OF_ETH_DST[], mod_dl_src:${ROUTE_MAC}, move:NXM_NX_ARP_SHA[]->NXM_NX_ARP_THA[], move:NXM_OF_ARP_SPA[]->NXM_OF_ARP_TPA[], load:$ROUTE_MAC_HEX->NXM_NX_ARP_SHA[], load:$ROUTE_IP_HEX->NXM_OF_ARP_SPA[], in_port"
 
-# ping virtual route
-ovs-ofctl add-flow $BR_INT table=0,in_port=$REP,icmp,nw_dst=$VM_ROUTE_IP,icmp_type=8,icmp_code=0,actions=push:"NXM_OF_ETH_SRC[]",push:"NXM_OF_ETH_DST[]",pop:"NXM_OF_ETH_SRC[]",pop:"NXM_OF_ETH_DST[]",push:"NXM_OF_IP_SRC[]",push:"NXM_OF_IP_DST[]",pop:"NXM_OF_IP_SRC[]",pop:"NXM_OF_IP_DST[]",load:"0xff->NXM_NX_IP_TTL[]",load:"0->NXM_OF_ICMP_TYPE[]",in_port
-ovs-ofctl add-flow $BR_INT table=0,in_port=$PATCH_INT,icmp,nw_dst=$ROUTE_IP,icmp_type=8,icmp_code=0,actions=push:"NXM_OF_ETH_SRC[]",push:"NXM_OF_ETH_DST[]",pop:"NXM_OF_ETH_SRC[]",pop:"NXM_OF_ETH_DST[]",push:"NXM_OF_IP_SRC[]",push:"NXM_OF_IP_DST[]",pop:"NXM_OF_IP_SRC[]",pop:"NXM_OF_IP_DST[]",load:"0xff->NXM_NX_IP_TTL[]",load:"0->NXM_OF_ICMP_TYPE[]",in_port
-
-ovs-ofctl add-flow $BR_EX "table=0,priority=50,in_port=$PATCH_EX,ip,nw_dst=$REMOTE_PF_IP,dl_dst=$MAC_BR_EX actions=mod_dl_dst:$MAC_REMOTE_PF,output:NORMAL"
-
-# none vxlan for vm2
-ovs-ofctl add-flow $BR_INT "table=0,priority=200,in_port=$REP2 actions=output:$PATCH_INT"
-ovs-ofctl add-flow $BR_EX "table=0,in_port=$PF,dl_dst=$VF2_MAC actions=output:$PATCH_EX"
-ovs-ofctl add-flow $BR_INT "table=0,priority=100,dl_dst=$VF2_MAC actions=output:$REP2"
-
 # DNAT
 # within vm, iperf -s, default port 5001
 # iperf -c 8.9.10.10 -p 9999
@@ -121,14 +110,54 @@ ovs-ofctl add-flow $BR_INT "table=0,priority=100,dl_dst=$VF2_MAC actions=output:
 IPERF_PORT=5001
 IPERF_PORT=4000
 NEW_PORT=9999
-ovs-ofctl add-flow $BR_EX "table=0,priority=100,in_port=$PF,tcp,tp_dst=$NEW_PORT,nw_dst=$ROUTE_IP actions=mod_nw_dst:$VM_IP,mod_tp_dst:$IPERF_PORT,mod_dl_dst=$VF_MAC,$PATCH_EX"
-ovs-ofctl add-flow $BR_INT "table=0,priority=110,tcp,tp_dst=$IPERF_PORT,nw_dst=$VM_IP actions=ct(table=2)"
-ovs-ofctl add-flow $BR_INT "table=2,priority=10,ct_state=+trk+new,ip actions=ct(commit),$REP"
-ovs-ofctl add-flow $BR_INT "table=2,priority=10,ct_state=+trk+est,ip actions=$REP"
+# ovs-ofctl add-flow $BR_EX "table=0,priority=100,in_port=$PF,tcp,tp_dst=$NEW_PORT,nw_dst=$ROUTE_IP actions=mod_nw_dst:$VM_IP,mod_tp_dst:$IPERF_PORT,mod_dl_dst=$VF_MAC,$PATCH_EX"
+# ovs-ofctl add-flow $BR_INT "table=0,priority=110,tcp,tp_dst=$IPERF_PORT,nw_dst=$VM_IP actions=ct(table=2)"
+# ovs-ofctl add-flow $BR_INT "table=2,priority=10,ct_state=+trk+new,ip actions=ct(commit),$REP"
+# ovs-ofctl add-flow $BR_INT "table=2,priority=10,ct_state=+trk+est,ip actions=$REP"
+# 
+# ovs-ofctl add-flow $BR_INT "table=0,priority=110,in_port=$REP,tcp,nw_src=$VM_IP,tp_src=$IPERF_PORT actions=ct(table=3)"
+# ovs-ofctl add-flow $BR_INT "table=3,priority=10,ct_state=+trk+new,ip actions=ct(commit),mod_nw_src:$ROUTE_IP,mod_tp_src:$NEW_PORT,mod_dl_dst=$MAC_REMOTE_PF,$PATCH_INT"
+# ovs-ofctl add-flow $BR_INT "table=3,priority=10,ct_state=+trk+est,ip actions=mod_nw_src:$ROUTE_IP,mod_tp_src:$NEW_PORT,mod_dl_dst=$MAC_REMOTE_PF,$PATCH_INT"
+# ovs-ofctl add-flow $BR_EX "table=0,priority=110,tcp,nw_src=$ROUTE_IP,tp_src=$NEW_PORT actions=output:$PF"
+
+
+# doesn't work
+# ovs-ofctl add-flow $BR_EX  "table=0,priority=100,in_port=$PF,tcp,tp_dst=$NEW_PORT,nw_dst=$ROUTE_IP actions=mod_nw_dst:$VM_IP,dec_ttl,ct(commit),mod_tp_dst:$IPERF_PORT,mod_dl_dst=$VF_MAC,dec_ttl,$PATCH_EX"
+
+# doesn't work
+ovs-ofctl add-flow $BR_EX  "table=0,priority=100,in_port=$PF,tcp,tp_dst=$NEW_PORT,nw_dst=$ROUTE_IP actions=dec_ttl,mod_nw_dst:$VM_IP,ct(commit),mod_tp_dst:$IPERF_PORT,mod_dl_dst=$VF_MAC,dec_ttl,$PATCH_EX"
+
+# 2021-12-16T03:03:13.528Z|00001|netdev_offload_tc(handler42)|ERR|netdev_tc_flow_put: 0: OVS_ACTION_ATTR_SET_MASKED
+# 2021-12-16T03:03:13.528Z|00002|netdev_offload_tc(handler42)|ERR|parse_put_flow_set_masked_action: type: 4, size: 12
+# 2021-12-16T03:03:13.528Z|00003|netdev_offload_tc(handler42)|ERR|netdev_tc_flow_put: 1: OVS_ACTION_ATTR_CT
+# 2021-12-16T03:03:13.528Z|00004|netdev_offload_tc(handler42)|ERR|netdev_tc_flow_put: 2: OVS_ACTION_ATTR_SET_MASKED
+# 2021-12-16T03:03:13.528Z|00005|netdev_offload_tc(handler42)|ERR|parse_put_flow_set_masked_action: type: 7, size: 12
+# 2021-12-16T03:03:13.528Z|00006|netdev_offload_tc(handler42)|ERR|netdev_tc_flow_put: 2: OVS_ACTION_ATTR_SET_MASKED
+# 2021-12-16T03:03:13.528Z|00007|netdev_offload_tc(handler42)|ERR|parse_put_flow_set_masked_action: type: 9, size: 4
+# 2021-12-16T03:03:13.528Z|00008|netdev_offload_tc(handler42)|ERR|netdev_tc_flow_put: 2: OVS_ACTION_ATTR_OUTPUT
+# works
+# ovs-ofctl add-flow $BR_EX  "table=0,priority=100,in_port=$PF,tcp,tp_dst=$NEW_PORT,nw_dst=$ROUTE_IP actions=mod_dl_dst=$VF_MAC,ct(commit),mod_tp_dst:$IPERF_PORT,mod_nw_dst:$VM_IP,dec_ttl,$PATCH_EX"
+
+# 2021-12-16T03:04:14.034Z|00001|netdev_offload_tc(handler80)|ERR|netdev_tc_flow_put: 0: OVS_ACTION_ATTR_SET_MASKED
+# 2021-12-16T03:04:14.034Z|00002|netdev_offload_tc(handler80)|ERR|parse_put_flow_set_masked_action: type: 7, size: 12
+# 2021-12-16T03:04:14.034Z|00003|netdev_offload_tc(handler80)|ERR|netdev_tc_flow_put: 1: OVS_ACTION_ATTR_CT
+# 2021-12-16T03:04:14.034Z|00004|netdev_offload_tc(handler80)|ERR|netdev_tc_flow_put: 2: OVS_ACTION_ATTR_SET_MASKED
+# 2021-12-16T03:04:14.034Z|00005|netdev_offload_tc(handler80)|ERR|parse_put_flow_set_masked_action: type: 4, size: 12
+# 2021-12-16T03:04:14.034Z|00006|netdev_offload_tc(handler80)|ERR|netdev_tc_flow_put: 2: OVS_ACTION_ATTR_SET_MASKED
+# 2021-12-16T03:04:14.034Z|00007|netdev_offload_tc(handler80)|ERR|parse_put_flow_set_masked_action: type: 7, size: 12
+# 2021-12-16T03:04:14.034Z|00008|netdev_offload_tc(handler80)|ERR|netdev_tc_flow_put: 2: OVS_ACTION_ATTR_SET_MASKED
+# 2021-12-16T03:04:14.034Z|00009|netdev_offload_tc(handler80)|ERR|parse_put_flow_set_masked_action: type: 9, size: 4
+# 2021-12-16T03:04:14.034Z|00010|netdev_offload_tc(handler80)|ERR|netdev_tc_flow_put: 2: OVS_ACTION_ATTR_OUTPUT
+# works, but dec_ttl is not executed
+# ovs-ofctl add-flow $BR_EX  "table=0,priority=100,in_port=$PF,tcp,tp_dst=$NEW_PORT,nw_dst=$ROUTE_IP actions=dec_ttl,ct(commit),mod_nw_dst:$VM_IP,mod_tp_dst:$IPERF_PORT,mod_dl_dst=$VF_MAC,$PATCH_EX"
+
+# works
+# ovs-ofctl add-flow $BR_EX  "table=0,priority=100,in_port=$PF,tcp,tp_dst=$NEW_PORT,nw_dst=$ROUTE_IP actions=mod_nw_dst:$VM_IP,ct(commit),mod_tp_dst:$IPERF_PORT,mod_dl_dst=$VF_MAC,$PATCH_EX"
+
+ovs-ofctl add-flow $BR_INT  "table=0,priority=10,ip actions=output:$REP"
 
 ovs-ofctl add-flow $BR_INT "table=0,priority=110,in_port=$REP,tcp,nw_src=$VM_IP,tp_src=$IPERF_PORT actions=ct(table=3)"
-ovs-ofctl add-flow $BR_INT "table=3,priority=10,ct_state=+trk+new,ip actions=ct(commit),mod_nw_src:$ROUTE_IP,mod_tp_src:$NEW_PORT,mod_dl_dst=$MAC_REMOTE_PF,$PATCH_INT"
-ovs-ofctl add-flow $BR_INT "table=3,priority=10,ct_state=+trk+est,ip actions=mod_nw_src:$ROUTE_IP,mod_tp_src:$NEW_PORT,mod_dl_dst=$MAC_REMOTE_PF,$PATCH_INT"
+ovs-ofctl add-flow $BR_INT "table=3,priority=10,ip actions=mod_nw_src:$ROUTE_IP,mod_tp_src:$NEW_PORT,mod_dl_dst=$MAC_REMOTE_PF,$PATCH_INT"
 ovs-ofctl add-flow $BR_EX "table=0,priority=110,tcp,nw_src=$ROUTE_IP,tp_src=$NEW_PORT actions=output:$PF"
 
 set +x
