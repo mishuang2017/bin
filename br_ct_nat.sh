2cf0ead2692520c291f21f27d00c59b94e244ae1
Chris Mi <cmi@nvidia.com>
2024-05-10 04:05:21 +0300

    update

--- br_ct_nat.sh
+++ br_ct_nat.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Do ct and ct_nat(commit)
+
 set -x
 
 br=br
@@ -49,9 +51,15 @@ for i in {1..1}; do
 	ovs-ofctl add-flow $br "table=0, in_port=$rep, dl_type=0x0806, nw_dst=192.168.0.254, actions=load:0x2->NXM_OF_ARP_OP[], move:NXM_OF_ETH_SRC[]->NXM_OF_ETH_DST[], mod_dl_src=${MAC_ROUTE}, move:NXM_NX_ARP_SHA[]->NXM_NX_ARP_THA[], move:NXM_OF_ARP_SPA[]->NXM_OF_ARP_TPA[], load:0x248a07ad7799->NXM_NX_ARP_SHA[], load:0xc0a800fe->NXM_OF_ARP_SPA[], in_port"
 	ovs-ofctl add-flow $br "table=0, in_port=$pf, dl_type=0x0806, nw_dst=8.9.10.1, actions=load:0x2->NXM_OF_ARP_OP[], move:NXM_OF_ETH_SRC[]->NXM_OF_ETH_DST[], mod_dl_src:${MAC_ROUTE}, move:NXM_NX_ARP_SHA[]->NXM_NX_ARP_THA[], move:NXM_OF_ARP_SPA[]->NXM_OF_ARP_TPA[], load:0x248a07ad7799->NXM_NX_ARP_SHA[], load:0x08090a01->NXM_OF_ARP_SPA[], in_port"
 
-	ovs-ofctl add-flow $br "table=0,in_port=$rep,priority=10,ip,ct_state=-trk,action=load:$reg6->NXM_NX_REG6[],ct(nat,table=10)"
-	ovs-ofctl add-flow $br "table=10,in_port=$rep,ip,reg6=$reg6,ct_state=+trk+new,action=ct(commit,nat(src=8.9.10.1:5000-50000),exec(move:NXM_NX_REG6[]->NXM_NX_CT_MARK[])),mod_dl_src:${MAC_ROUTE},mod_dl_dst:${REMOTE_PF_MAC},$pf"
-	ovs-ofctl add-flow $br "table=10,in_port=$rep,ct_state=+trk+est-rpl,ip,action=mod_dl_src:${MAC_ROUTE},mod_dl_dst:${REMOTE_PF_MAC},$pf"
+	ovs-ofctl add-flow $br "table=0,in_port=$rep,ip,ct_state=-trk             actions=ct(table=10)"
+        ovs-ofctl add-flow $br "table=10,in_port=$rep,ip,ct_state=+trk+new        actions=goto_table=20"
+	ovs-ofctl add-flow $br "table=10,in_port=$rep,ip,ct_state=+trk+est-rpl,   action=ct_clear,goto_table=20"
+
+	ovs-ofctl add-flow $br "table=20,in_port=$rep,ip,   actions=ct_clear,resubmit(,30)"
+
+	ovs-ofctl add-flow $br "table=30,in_port=$rep,priority=10,ip,ct_state=-trk,action=load:$reg6->NXM_NX_REG6[],ct(nat,table=40)"
+	ovs-ofctl add-flow $br "table=40,in_port=$rep,ip,reg6=$reg6,ct_state=+trk+new,action=ct(commit,nat(src=8.9.10.1:5000-50000),exec(move:NXM_NX_REG6[]->NXM_NX_CT_MARK[])),mod_dl_src:${MAC_ROUTE},mod_dl_dst:${REMOTE_PF_MAC},$pf"
+	ovs-ofctl add-flow $br "table=40,in_port=$rep,ct_state=+trk+est-rpl,ip,action=mod_dl_src:${MAC_ROUTE},mod_dl_dst:${REMOTE_PF_MAC},$pf"
 
 	ovs-ofctl add-flow $br "table=0,in_port=$pf,priority=10,ip,ct_state=-trk,action=ct(nat,table=20)"
 	ovs-ofctl add-flow $br "table=20,in_port=$pf,ct_mark=$reg6,ct_state=+trk+est+rpl,ip,action=mod_dl_src:${MAC_ROUTE},mod_dl_dst:${VF_MAC},$rep"
