7bed658e0a0c3d486cf4ca5a74af53929baea29a
Chris Mi <cmi@nvidia.com>
2026-05-18 08:57:22 +0300

    update

--- sf_create.sh
+++ sf_create.sh
@@ -1,4 +1,4 @@
-for i in {0..99}; do
+for i in {0..49}; do
     sf_id=$(/opt/mellanox/iproute2/sbin/mlxdevm port add pci/0000:03:00.0 flavour pcisf pfnum 0 sfnum $i controller 1 | grep "pci/" | awk '{print $1}' | sed 's/:$//')
     if [ -n "$sf_id" ]; then
         echo "SF $i ($sf_id) -> Created"
